259d53f24b1295a7de31aa553f3f1b1575385749
Jay Sanghavi <jay.sanghavi@outlook.com>
2023-03-05 21:29:45 +0530

    Varaible change

--- Day 1.py
+++ Day 1.py
@@ -3,7 +3,7 @@ print("print('This is how to print')")
 print("Hello world \nHello world")
 print("Hello " + "Jay")
 input("What is your name?")
-print("Hello" + input("What is your name?"))
+print("Hello " + input("What is your name?"))
 print(len(input("What is your name? ")))
 name = "Jay"
 print(name)
@@ -11,3 +11,11 @@ name = "Angela"
 print(name)
 length = len(name)
 print(length)
+
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a = " + a)
+print("b = " + b)
