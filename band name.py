8be00167fedb6ae55ebcb6903173d3b3e7f60d41
Jay Sanghavi <jay.sanghavi@outlook.com>
2023-03-08 17:52:51 +0530

    minor changes

--- band name.py
+++ band name.py
@@ -1,4 +1,4 @@
 print("Welocme to band name genrator")
-city_name = input("What is your city name?\n")
-pet_name = input("What is your pet name?\n")
+city_name = input("What is your city name? \n")
+pet_name = input("What is your pet name? \n")
 print("Your band name could be " + city_name + " " + pet_name)
